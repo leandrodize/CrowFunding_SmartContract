8c02947dc6b2b52f420f06926152ffc56685213e
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-12-01 10:12:26 -0500

    Local MAIN: add enum FundrasingState {Open, Close}

--- contracts/crowdFunding.sol
+++ contracts/crowdFunding.sol
@@ -3,21 +3,27 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract CrowdFound {
+    enum FundrasingState {
+        Open, 
+        Close
+    }
+
     struct Project {
         string name;
         string description;
         address payable founder;
         uint funds;
         uint goal;
-        string state;
+        FundrasingState state;
     }
+    
     Project public project;
 
     event financedProject (address contributor, uint amount);
-    event modifiedProject (address modifiers, string status);
+    event modifiedProject (address modifiers, FundrasingState status);
 
     constructor(string memory _name, string memory _description, uint _goal){
-        project = Project(_name, _description, payable(msg.sender), 0, _goal, "Opened");
+        project = Project(_name, _description, payable(msg.sender), 0, _goal, FundrasingState.Open);
     }
 
     modifier excluyeFounder(){
@@ -31,12 +37,13 @@ contract CrowdFound {
     }
 
     function fundProject() public payable excluyeFounder{
+        require(project.state == FundrasingState.Open, "The project can not receive funds");
         project.founder.transfer(msg.value);
         project.funds = project.funds + msg.value;
         emit financedProject (msg.sender, msg.value);
     }
 
-    function changeProjectState(string calldata newState ) public onlyFounder{
+    function changeProjectState(FundrasingState newState ) public onlyFounder{
         project.state = newState;
         emit modifiedProject(msg.sender, project.state);
     }
