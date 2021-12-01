ad227aed64956aa2a6a17c77784ce400880edb44
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-12-01 09:48:11 -0500

    Local MAIN: add structure contract

--- contracts/crowdFunding.sol
+++ contracts/crowdFunding.sol
@@ -3,37 +3,41 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract CrowdFound {
-    string public name;
-    string public description;
-    address payable public founder;
-    uint public funds;
-    uint public goal;
-    string public state = "Opened";
+    struct Project {
+        string name;
+        string description;
+        address payable founder;
+        uint funds;
+        uint goal;
+        string state;
+    }
+    Project public project;
 
     event financedProject (address contributor, uint amount);
     event modifiedProject (address modifiers, string status);
 
     constructor(string memory _name, string memory _description, uint _goal){
-        name = _name;
-        description = _description;
-        goal = _goal;
-        founder = payable(msg.sender);
+        project = Project(_name, _description, payable(msg.sender), 0, _goal, "Opened");
     }
+
     modifier excluyeFounder(){
-        require(founder != msg.sender , "The founder cannot invest here");
+        require(project.founder != msg.sender , "The founder cannot invest here");
         _;
     }
+
     modifier onlyFounder(){
-        require(founder == msg.sender , "Only Founder can modifier state Project");
+        require(project.founder == msg.sender , "Only Founder can modifier state Project");
         _;
     }
+
     function fundProject() public payable excluyeFounder{
-        founder.transfer(msg.value);
-        funds = funds + msg.value;
+        project.founder.transfer(msg.value);
+        project.funds = project.funds + msg.value;
         emit financedProject (msg.sender, msg.value);
     }
+
     function changeProjectState(string calldata newState ) public onlyFounder{
-        state = newState;
-        emit modifiedProject(msg.sender, state);
+        project.state = newState;
+        emit modifiedProject(msg.sender, project.state);
     }
 }
